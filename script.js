86c87b65ccc5c2751b11e6af07a46a4f2ffd67e0
myrajain710-alt <myra.jain710@gmail.com>
2026-01-18 11:13:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const textarea = document.querySelector("textarea");
-const counter = document.querySelector(".post-footer span");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-textarea.addEventListener("input", () => {
-  counter.textContent = `${textarea.value.length}/1280`;
+let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+const feed = document.createElement("div");
+document.body.appendChild(feed);
+
+function renderPosts() {
+  feed.innerHTML = "";
+  posts.forEach(text => {
+    const p = document.createElement("p");
+    p.textContent = text;
+    p.style.padding = "10px";
+    p.style.margin = "10px 0";
+    p.style.background = "#1f1f1f";
+    p.style.borderRadius = "8px";
+    feed.appendChild(p);
+  });
+}
+
+button.addEventListener("click", () => {
+  if (input.value.trim() === "") return;
+
+  posts.unshift(input.value);
+  localStorage.setItem("posts", JSON.stringify(posts));
+  input.value = "";
+  renderPosts();
 });
+
+renderPosts();
+
+
+
 
